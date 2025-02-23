bb286001719d84e8ca7342a8697325ad5e8f4573
RakibRahman <md.rr.talukder@gmail.com>
2025-02-23 21:52:21 +0600

    maps in go

--- maps/maps.go
+++ maps/maps.go
@@ -1,6 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"maps"
+)
+
+func mapAsSet() {
+	intSet := map[int]bool{}
+	vals := []int{5, 10, 2, 5, 8, 7, 3, 9, 1, 2, 10}
+
+	for _, v := range vals {
+		intSet[v] = true
+	}
+	fmt.Println(intSet)
+	fmt.Println(len(vals), len(intSet))
+	fmt.Println(intSet[5])
+	fmt.Println(intSet[500])
+	if intSet[100] {
+		fmt.Println("100 is in the set")
+	}
+}
 
 func main() {
 	var nilMap map[string]int
@@ -14,5 +33,47 @@ func main() {
 	fmt.Println(nilMap)
 	fmt.Println(totalWins)
 	fmt.Println(schoolInfo)
+	fmt.Println(len(schoolInfo))
+	totalWins["Orcas"] = 1
+	totalWins["Lions"] = 2
+	fmt.Println(totalWins["Orcas"])
+	fmt.Println(totalWins["Kittens"])
+	totalWins["Kittens"]++
+	fmt.Println(totalWins["Kittens"])
+	totalWins["Lions"] = 3
+	fmt.Println(totalWins["Lions"])
 
+	m := map[string]int{
+		"hello": 5,
+		"world": 0,
+		"code":  9,
+	}
+
+	//The comma ok Idiom - to check if a key is present in the map
+	v, ok := m["hello"]
+	fmt.Println(v, ok) // 5 true
+	v, ok = m["goodbye"]
+	fmt.Println(v, ok) //0 false
+
+	// Deleting from Maps
+	delete(m, "hello")
+	fmt.Println(m)
+
+	fmt.Println(m, len(m))
+	clear(m) //Emptying a Map
+	fmt.Println(m, len(m))
+
+	m = map[string]int{
+		"hello": 5,
+		"world": 10,
+	}
+
+	n := map[string]int{
+		"world": 10,
+		"hello": 5,
+	}
+
+	fmt.Println(maps.Equal(m, n)) //Comparing Maps
+
+	mapAsSet()
 }
