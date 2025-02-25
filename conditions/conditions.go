baf78273be004abd1218f5f6231ceaf20ae22587
RakibRahman <md.rr.talukder@gmail.com>
2025-02-25 08:43:26 +0600

    Scoping a variable to an if statement

--- conditions/conditions.go
+++ conditions/conditions.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func checkAge(age int) {
 	if age >= 18 {
@@ -12,8 +15,34 @@ func checkAge(age int) {
 	}
 }
 
+func randomNumber() {
+	n := rand.Intn(10)
+
+	if n == 0 {
+		fmt.Printf("%d is a low number", n)
+	} else if n > 5 {
+		fmt.Printf("%d is a big number", n)
+	} else {
+		fmt.Printf("%d is a good number", n)
+	}
+}
+
+// Scoping a variable to an if statement
+func randomNumberv2() {
+
+	if n := rand.Intn(10); n == 0 {
+		fmt.Printf("%d is a low number", n)
+	} else if n > 5 {
+		fmt.Printf("%d is a big number", n)
+	} else {
+		fmt.Printf("%d is a good number", n)
+	}
+
+}
 func main() {
 	checkAge(16)
 	checkAge(18)
 	checkAge(8)
+	randomNumber()
+	randomNumberv2()
 }
